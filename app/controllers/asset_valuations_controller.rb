205a87d7ecc70af2c24aab88d07b2941e81eabed
Thomas Maurer <t@maurer.id>
2026-01-27 13:52:59 +0100

    Include archived assets in valuations form totals
    Group subtotals and Total Net Worth now sum both active and archived
    assets instead of only active assets.

--- app/controllers/asset_valuations_controller.rb
+++ app/controllers/asset_valuations_controller.rb
@@ -123,13 +123,13 @@ class AssetValuationsController < ApplicationController
 
   def build_group_totals_by_month
     # Build a hash: { group_id => { date => net_value } }
-    # Only includes active (non-archived) assets
+    # Includes all assets (active and archived)
     totals = Hash.new { |h, k| h[k] = {} }
 
     @asset_groups.each do |group|
       @months.each do |month|
         net = 0
-        group.assets.active.each do |asset|
+        group.assets.each do |asset|
           value = @valuations_by_asset_and_month[asset.id][month]
           next unless value
 
