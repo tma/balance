715112e3d7f97a6a0c41beeafc5567803eca25fa
Thomas Maurer <t@maurer.id>
2026-02-04 20:59:51 +0100

    Add month navigation arrows and keyboard shortcuts to cash flow page
    - Add left/right arrows to navigate between months
    - Arrows work from Year view (goes to Dec prev year / Jan current year)
    - Add keyboard shortcuts: Arrow keys and j/k for month navigation
    - Use fetch + morph approach for smooth navigation without scroll jump
    - All navigation links (Year, months, arrows) use the same smooth approach

---
+++ app/javascript/controllers/month_navigation_controller.js
@@ -0,0 +1,83 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    prevUrl: String,
+    nextUrl: String
+  }
+
+  connect() {
+    this.handleKeydown = this.handleKeydown.bind(this)
+    document.addEventListener("keydown", this.handleKeydown)
+  }
+
+  disconnect() {
+    document.removeEventListener("keydown", this.handleKeydown)
+  }
+
+  handleKeydown(event) {
+    // Don't trigger if user is typing in an input/textarea
+    if (event.target.matches("input, textarea, select, [contenteditable]")) {
+      return
+    }
+
+    // Don't trigger if modifier keys are pressed (except shift for some cases)
+    if (event.metaKey || event.ctrlKey || event.altKey) {
+      return
+    }
+
+    if ((event.key === "ArrowLeft" || event.key === "k") && this.hasPrevUrlValue) {
+      event.preventDefault()
+      this.navigate(this.prevUrlValue)
+    } else if ((event.key === "ArrowRight" || event.key === "j") && this.hasNextUrlValue) {
+      event.preventDefault()
+      this.navigate(this.nextUrlValue)
+    }
+  }
+
+  navigateLink(event) {
+    event.preventDefault()
+    const url = event.currentTarget.href
+    this.navigate(url)
+  }
+
+  navigate(url) {
+    // Use fetch + morphing to update content without scroll jump
+    fetch(url, {
+      headers: {
+        "Accept": "text/html",
+        "Turbo-Frame": "_top"
+      }
+    })
+    .then(response => response.text())
+    .then(html => {
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(html, "text/html")
+      const newContent = doc.querySelector("[data-controller='month-navigation']")
+      
+      if (newContent) {
+        // Update URL without scrolling
+        history.pushState({}, "", url)
+        
+        // Morph the content
+        this.element.innerHTML = newContent.innerHTML
+        
+        // Update controller values
+        const prevUrl = newContent.dataset.monthNavigationPrevUrlValue
+        const nextUrl = newContent.dataset.monthNavigationNextUrlValue
+        
+        if (prevUrl) {
+          this.prevUrlValue = prevUrl
+        } else {
+          delete this.element.dataset.monthNavigationPrevUrlValue
+        }
+        
+        if (nextUrl) {
+          this.nextUrlValue = nextUrl
+        } else {
+          delete this.element.dataset.monthNavigationNextUrlValue
+        }
+      }
+    })
+  }
+}
